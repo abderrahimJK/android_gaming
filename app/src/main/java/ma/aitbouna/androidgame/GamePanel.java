9b3023104f348f957eef19eea01b9d6700620ca4
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-04-06 06:16:12 +0000

    draw a simple rectangle with surfaceView

--- app/src/main/java/ma/aitbouna/androidgame/GamePanel.java
+++ app/src/main/java/ma/aitbouna/androidgame/GamePanel.java
@@ -1,6 +1,9 @@
 package ma.aitbouna.androidgame;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -8,13 +11,18 @@ import androidx.annotation.NonNull;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
+    private Paint redPaint = new Paint();
     public GamePanel(Context context) {
         super(context);
+        getHolder().addCallback(this);
+        redPaint.setColor(Color.RED);
     }
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
-
+        Canvas c = holder.lockCanvas();
+        c.drawRect(50, 50, 1000, 1000, redPaint);
+        holder.unlockCanvasAndPost(c);
     }
 
     @Override
