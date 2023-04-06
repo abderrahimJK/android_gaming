9ccf2be2bc815f1b112d94908195e140b8944ff1
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-04-06 06:02:08 +0000

    project setup

---
+++ app/src/main/java/ma/aitbouna/androidgame/GamePanel.java
@@ -0,0 +1,29 @@
+package ma.aitbouna.androidgame;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+    public GamePanel(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+}
